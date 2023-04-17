ee1f2553430f7abd5cc9ccea0e7008c7662853cb
giant <giantfedor@gmail.com>
2023-04-17 23:32:17 +0300

    Изменение параметров

---
+++ Special/big_variables.py
@@ -0,0 +1,7 @@
+user_values_dict = {1: 'login',
+                    2: 'email',
+                    3: 'hashed_password',
+                    4: 'status',
+                    5: 'telegram_id',
+                    6: 'current_id',
+                    7: 'history_id'}
