c59cd5012ce53f766a450c646beb97187f716ebe
giant <giantfedor@gmail.com>
2023-04-20 22:38:56 +0300

    fix

--- API/data/users.py
+++ API/data/users.py
@@ -12,11 +12,11 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     status = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     telegram_id = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
-    current_id = sqlalchemy.Column(sqlalchemy.String, index=True, nullable=True)
-    history_id = sqlalchemy.Column(sqlalchemy.String, index=True, nullable=True)
+    current_id = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    history_id = sqlalchemy.Column(sqlalchemy.String, nullable=True)
 
     goods = orm.relationship("Goods", back_populates='user')
 
     def __repr__(self):
         return f'{self.id} *** {self.login} *** {self.email} *** {self.hashed_password} *** {self.status} *** ' \
-               f'{self.telegram_id} *** f{self.current_id} *** f{self.history_id}'
+               f'{self.telegram_id} *** {self.current_id} *** {self.history_id}'
